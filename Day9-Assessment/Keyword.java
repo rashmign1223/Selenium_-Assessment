699eed70ff839793c519d3769d66b7cdaaa439cb
rashmign1223 <167083335+rashmign1223@users.noreply.github.com>
2026-09-16 22:40:24 +0530

    Keyword.java

---
+++ Day9-Assessment/Keyword.java
@@ -0,0 +1,13 @@
+package Day9Assessment;
+
+public class Keywordexe{
+	public static void main(String[] args) {
+		SauseExecutor s = new SauseExecutor(); 
+		s.exe("LAUNCH_BROWSER"); 
+		s.exe("OPEN_URL"); 
+		s.exe("USERNAME"); 
+		s.exe("PASS"); 
+		s.exe("LBUTTON"); 
+		
+	}
+}
